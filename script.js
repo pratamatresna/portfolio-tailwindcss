b58f13ad4bb3fea2a3491746e9d82677e36324e4
magungpratamat <pratama.tresna08@gmail.com>
2023-11-05 19:37:33 +0700

    Update last

--- script.js
+++ script.js
@@ -33,3 +33,27 @@ window.addEventListener("click", function (e) {
     navMenu.classList.add("hidden");
   }
 });
+
+// dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindahkan toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
