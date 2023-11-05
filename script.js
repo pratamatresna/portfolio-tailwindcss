0d44deaf65235cc2c60bb50cc94c6140f24a902d
magungpratamat <pratama.tresna08@gmail.com>
2023-11-05 14:17:46 +0700

    update

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ window.onscroll = function () {
   const header = document.querySelector("header");
   //jadi nilai fixednav ini pasti 0 y nya
   const fixedNav = header.offsetTop;
+  // mengambil id to-top
+  const toTop = document.querySelector("#to-top");
   //ketika window scroll lebih besar dari fixedNAv kalau ada selisih maka tambah kelas navbar-fixed
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.add("flex");
+    toTop.classList.remove("hidden");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
   }
 };
 
@@ -18,4 +24,12 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   //jadi kalau ada kelas nya akan di hapus kalau ga ada maka tambah
   navMenu.classList.toggle("hidden");
+});
+
+// klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
 });
